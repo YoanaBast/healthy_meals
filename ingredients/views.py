2a9aa4efde66722083031481ffe5371969416242
Yoana Bastiyanova <bastianovajoana@gmail.com>
2026-01-21 16:49:10 +0200

    built prototype for ingredients page, reworked the svg background for the homepage, added more to-dos in basic

--- ingredients/views.py
+++ ingredients/views.py
@@ -2,3 +2,6 @@ from django.shortcuts import render
 
 def manage_ingredients(request):
     return render(request, 'ingredients/manage_ingredients.html')
+
+def create_ingredient_more(request):
+    return render(request, 'ingredients/more_ingredient_creation.html')
