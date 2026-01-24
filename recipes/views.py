67cde3e8e62e8e4cf3afcdc6332e8100f950a07a
Yoana Bastiyanova <bastianovajoana@gmail.com>
2026-01-24 22:03:10 +0200

    liked the templates in ingredients and recipes to real data with DTL

--- recipes/views.py
+++ recipes/views.py
@@ -1,6 +1,17 @@
 from django.shortcuts import render
+from .models import Recipe, RecipeCategory
 
 # Create your views here.
 
 def manage_recipes(request):
-    return render(request, 'recipes/manage_recipes.html')
+    recipe = Recipe.objects.all()
+    categories = RecipeCategory.objects.all()
+
+    context = {
+        "recipe": recipe,
+        "category": categories,
+
+    }
+
+    return render(request, 'recipes/manage_recipes.html', context)
+
