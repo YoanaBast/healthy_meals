8e4b0f63dd6ad1b5e8e9e7103a36412d40fa28bf
Yoana Bastiyanova <bastianovajoana@gmail.com>
2026-02-05 12:17:32 +0200

    added fridge model and registered in admin

--- planner/admin.py
+++ planner/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
 
+from planner.models import Fridge
+
+
 # Register your models here.
+
+@admin.register(Fridge)
+class FridgeAdmin(admin.ModelAdmin):
+    list_display = ('ingredient', 'quantity', 'unit')
+    search_fields = ('ingredient',)
+
+
