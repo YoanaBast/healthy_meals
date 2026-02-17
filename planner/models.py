1a8365c76f02c40e0a346ed498398dd1f59aef6f
Yoana Bastiyanova <bastianovajoana@gmail.com>
2026-02-17 02:19:24 +0200

    added add item to fridge option

--- planner/models.py
+++ planner/models.py
@@ -2,21 +2,20 @@ from django.conf import settings
 from django.db import models
 
 from ingredients.models import Ingredient, IngredientMeasurementUnit, MeasurementUnit
+from recipes.models import Recipe
 
 
 # Create your models here.
 
+
+
 class UserFridge(models.Model):
     user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
-    def __str__(self):
-        return f"{self.user.username}'s Fridge"
-
-class Fridge(models.Model):
-    user_fridge = models.ForeignKey(UserFridge, on_delete=models.CASCADE, related_name='items')
     ingredient = models.ForeignKey(Ingredient, on_delete=models.CASCADE)
     quantity = models.FloatField(default=0)
     unit = models.ForeignKey(MeasurementUnit, on_delete=models.SET_NULL, null=True)
 
     def __str__(self):
-        return f"{self.ingredient.name} ({self.quantity} {self.unit})"
+        return f"{self.user.username} - {self.ingredient.name}"
+
 
--- planner/views.py
+++ planner/views.py
@@ -1,3 +1,67 @@
-from django.shortcuts import render
+from django.contrib.auth.models import User
+from django.shortcuts import render, get_object_or_404, redirect
+
+from ingredients.models import Ingredient, MeasurementUnit
+from planner.models import UserFridge
+
 
 # Create your views here.
+
+def manage_fridge(request):
+    user = User.objects.get(username="default")
+    fridge = UserFridge.objects.filter(user=user)
+    ingredients = Ingredient.objects.all()
+
+
+    context = {
+        'fridge': fridge,
+        'ingredients': ingredients,
+
+    }
+    return render(request, 'planner/manage_fridge.html', context)
+
+def edit_fridge_item(request, item_id):
+    item = get_object_or_404(UserFridge, id=item_id)
+
+    if request.method == "POST":
+        item.quantity = request.POST.get("quantity", item.quantity)
+        item.unit = request.POST.get("unit", item.unit)
+        item.save()
+        return redirect('manage_fridge')
+
+    context = {
+        'item': item,
+    }
+    return render(request, 'planner/edit_fridge_item.html', context)
+
+
+
+def add_fridge_item(request):
+    if request.method == "POST":
+        user, _ = User.objects.get_or_create(username="default")
+
+        ing_id = request.POST.get("ingredient_id")
+        qty = float(request.POST.get("quantity"))
+        unit_id = request.POST.get("unit")
+
+        ingredient = Ingredient.objects.get(id=ing_id)
+        unit = MeasurementUnit.objects.get(id=unit_id)
+
+        existing_item = UserFridge.objects.filter(
+            user=user,
+            ingredient=ingredient,
+            unit=unit
+        ).first()
+
+        if existing_item:
+            existing_item.quantity += qty
+            existing_item.save()
+        else:
+            UserFridge.objects.create(
+                user=user,
+                ingredient=ingredient,
+                quantity=qty,
+                unit=unit
+            )
+
+    return redirect('manage_fridge')
