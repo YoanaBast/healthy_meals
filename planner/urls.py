af862c7f84b95b77139e9ee1300701b79ff0d6dc
Yoana Bastiyanova <bastianovajoana@gmail.com>
2026-02-21 04:01:01 +0200

    added grocery list model, view, template for the generate grocery list to save into

--- planner/urls.py
+++ planner/urls.py
@@ -10,5 +10,8 @@ urlpatterns = [
     path('suggestions/', views.get_meal_suggestions, name='meal_suggestions'),
     path('recipes/<int:id>/make/', views.make_recipe, name='make_recipe'),
     path('generate_grocery_list/', views.generate_grocery_list, name='generate_grocery_list'),
-
+    path('grocery-list/', views.user_grocery_list, name='user_grocery_list'),
+    path("grocery-list/delete/<int:item_id>/", views.delete_grocery_item, name="delete_grocery_item"),
+    path("grocery-list/add-to-fridge/<int:item_id>/", views.add_grocery_to_fridge, name="add_grocery_to_fridge",),
+    path("grocery-list/add-all-to-fridge/", views.add_all_grocery_to_fridge, name="add_all_grocery_to_fridge",),
 ]
