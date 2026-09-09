52a2392f330de58e9ffcd115d500f294be717ea9
Yoana Bastiyanova <fordemaciarrr@gmail.com>
2026-09-09 03:08:43 +0300

    Update settings.py

--- meals_manager/settings.py
+++ meals_manager/settings.py
@@ -78,12 +78,12 @@ PROJECT_APPS = [
 
 from datetime import timedelta
 
-SIMPLE_JWT = {
-    'ACCESS_TOKEN_LIFETIME': timedelta(minutes=30),
-    'REFRESH_TOKEN_LIFETIME': timedelta(days=7),
-    'ROTATE_REFRESH_TOKENS': True,
-    'BLACKLIST_AFTER_ROTATION': True,
-}
+# SIMPLE_JWT = {
+#     'ACCESS_TOKEN_LIFETIME': timedelta(minutes=30),
+#     'REFRESH_TOKEN_LIFETIME': timedelta(days=7),
+#     'ROTATE_REFRESH_TOKENS': True,
+#     'BLACKLIST_AFTER_ROTATION': True,
+# }
 
 INSTALLED_APPS = [
     'jazzmin',
